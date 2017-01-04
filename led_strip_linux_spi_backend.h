d33e3547add9d5aed524c8cfc25fd4c31052335b
Thomas DeRensis <tderensis@gmail.com>
2017-01-04 01:04:40 -0500

    fix formatting

--- led_strip_linux_spi_backend.h
+++ led_strip_linux_spi_backend.h
@@ -2,10 +2,9 @@
 #ifndef LED_STRIP_LINUX_SPI_BACKEND_H
 #define LED_STRIP_LINUX_SPI_BACKEND_H
 
-//extern led_strip_t;
 #include "led_strip.h"
 
 led_strip_t * led_strip_create_linux_spi(const char * device,
-										 uint32_t frequency,
-										 uint32_t num_leds);
+                                         uint32_t frequency,
+                                         uint32_t num_leds);
 #endif
