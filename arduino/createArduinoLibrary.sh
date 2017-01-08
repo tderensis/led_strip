dcb43908e12b75520a9f70cf0bc427d4529cf9bb
Thomas DeRensis <tderensis@gmail.com>
2017-01-07 21:18:59 -0500

    Restructure the Arduino code so that it can be installed as a library using createArduinoLibrary.sh

--- arduino/createArduinoLibrary.sh
+++ arduino/createArduinoLibrary.sh
@@ -6,3 +6,6 @@ cp ../led_strip_no_backend.c led_strip_no_backend.cpp
 cp ../led_strip_no_backend.h .
 cp ../led_strip-cpp.h .
 cp ../led_strip-cpp-implementation.h .
+cp ../led_strip_struct.h .
+
+zip -r LedStrip.zip * -x setup.sh
