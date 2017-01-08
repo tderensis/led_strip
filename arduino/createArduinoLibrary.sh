190dbe9fea7ca47e2f24f93805dac1896ede839a
Thomas DeRensis <tderensis@gmail.com>
2017-01-08 17:50:02 -0500

    Fix paths for the arduino library zip.

--- arduino/createArduinoLibrary.sh
+++ arduino/createArduinoLibrary.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cp ../led_strip.h .
-cp ../led_strip.c led_strip.cpp
-cp ../led_strip_no_backend.c led_strip_no_backend.cpp
-cp ../led_strip_no_backend.h .
-cp ../led_strip-cpp.h .
-cp ../led_strip-cpp-implementation.h .
-cp ../led_strip_struct.h .
+cp ../src/led_strip.h .
+cp ../src/led_strip.c led_strip.cpp
+cp ../src/led_strip_no_backend.c led_strip_no_backend.cpp
+cp ../src/led_strip_no_backend.h .
+cp ../src/led_strip-cpp.h .
+cp ../src/led_strip-cpp-implementation.h .
+cp ../src/led_strip_struct.h .
 
-zip -r LedStrip.zip * -x setup.sh
+zip -r LedStrip.zip * -x createArduinoLibrary.sh
